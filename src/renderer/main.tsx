b51a5996b2021f71aae3021da09e099c49cd69dc
PolarisPyra <87876386+PolarisPyra@users.noreply.github.com>
2026-05-14 16:17:02 -0400

    Install shadcn and Tailwind v4

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 
+import { TooltipProvider } from "@/components/ui/tooltip"
+
 import { App } from "./App"
 import "./styles.css"
 
 createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<TooltipProvider>
+			<App />
+		</TooltipProvider>
 	</React.StrictMode>
 )
